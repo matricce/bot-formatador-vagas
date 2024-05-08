b368753042ecb61d415789f51ae8c799fcbe7fc9
Ruvian S. <12111730+matricce@users.noreply.github.com>
2024-05-07 22:19:16 -0300

    fix: ajuste na função de captura de dados de links

--- src/filters/retrieveContent.ts
+++ src/filters/retrieveContent.ts
@@ -3,24 +3,53 @@ import { execSync } from 'child_process';
 import * as cheerio from 'cheerio';
 import { RetrieveContentResponse } from '../types/shared-interfaces';
 
+const withAxios = async url => {
+  const data = await axios
+    .get(url)
+    .then(res => (res.data.widget ? undefined : res.data))
+    .catch(() => undefined);
+  data && console.log('Retrieved content from url', url, 'using axios');
+  return data;
+};
+
+const withProxy = async url => {
+  const data = await axios
+    .get(`https://api.allorigins.win/get?url=${url}`)
+    .then(res => res.data)
+    .catch(() => undefined);
+  data &&
+    console.log(
+      'Retrieved content from url',
+      url,
+      'using allorigins, code:',
+      data?.status?.http_code,
+      'response time: ',
+      data?.status?.response_time,
+    );
+  return data?.contents;
+};
+
+const withCurl = url => {
+  const data = execSync(`
+  curl '${url}' \
+  -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
+  -H 'sec-ch-ua: "Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"' \
+  -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
+  `).toString('utf-8');
+  data && console.log('Retrieved content from url', url, 'using curl');
+  return data;
+};
+
 export const retrieveContent = async (url: string): Promise<RetrieveContentResponse> => {
   console.log('Retrieving content from url:', url);
   url = url.startsWith('https://') ? url : 'https://' + url;
-  const response = (await axios
-    .get(url)
-    .then(res => (res.data.widget ? undefined : res))
-    .catch(() => undefined)) || {
-    data: execSync(`
-    curl '${url}' \
-    -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7' \
-    -H 'sec-ch-ua: "Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"' \
-    -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'
-    `).toString('utf-8'),
-  };
+  const data = (await withAxios(url)) || (await withProxy(url)) || withCurl(url);
 
-  const $ = cheerio.load(response.data);
+  const $ = cheerio.load(data);
   const jobObject = $('script').attr('type', 'application/ld+json');
-  const jobObjectData = JSON.parse((<any>jobObject?.[0]?.children?.[0])?.data);
+  const jobObjectData = await Promise.resolve((<any>jobObject?.[0]?.children?.[0])?.data)
+    .then(JSON.parse)
+    .catch(() => undefined);
   const jobObjectTitle = jobObjectData?.title;
   const jobObjectBody = jobObjectData?.description;
   const jobTitle = $('title').html() || jobObjectTitle || '';
