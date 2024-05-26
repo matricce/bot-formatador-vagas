074e7d61e9c67f889cba1640bfb5ac6bcdba7f7d
Ruvian S. <12111730+matricce@users.noreply.github.com>
2024-05-26 19:24:04 -0300

    refactor: ajustes no script de postbuild

--- postbuild-set-webhook.sh
+++ postbuild-set-webhook.sh
@@ -1,7 +1,9 @@
-# !/bin/bash
+#!/bin/bash
+
+[[ -z $VERCEL_URL ]] && VERCEL_URL=$WEBHOOK_URL
 
 # Chama a API do Telegram para configurar o webhook
-echo "Setting Webhook..."
+echo "Setting Webhook to $VERCEL_URL/api/index"
 curl -X POST \
   -H "Content-Type: application/json" \
   -d "{\"url\": \"$VERCEL_URL/api/index\"}" \
