fed9ea67e9ccf1e701855cbabcb27098a0dbbfa7
Ruvian S. <12111730+matricce@users.noreply.github.com>
2024-06-02 00:32:03 -0300

    fix: tratamento de tipos de mensagens não aceitos

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import bot from './bot';
 import commands from './commands/composer';
 import { checkText } from './functions';
+import { erroValidaUrl } from './responses/messages';
 import { processLink, processMenuResponse } from './utils';
 import { development } from './utils/start';
 
@@ -12,6 +13,8 @@ bot.on(['::url', '::text_link'], processLink);
 bot.on(':text', checkText);
 
 bot.on('callback_query:data', processMenuResponse);
+
+bot.on('message', ctx => ctx.reply(erroValidaUrl, { reply_to_message_id: ctx.msg?.message_id }));
 
 bot.catch((e: Error) => console.error(e));
 
